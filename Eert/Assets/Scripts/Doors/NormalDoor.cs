7c3a8b6a07360ca9e328ab40ce1892b12b473bf6
Steven Alexander <stevena1@usc.edu>
2022-04-02 14:34:26 -0700

    door function

---
+++ Eert/Assets/Scripts/Doors/NormalDoor.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalDoor : MonoBehaviour
+{
+
+    [SerializeField] private float timeToBreak;
+    [SerializeField] private float currentTime = 0.0f;
+    private bool hasCollided = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentTime >= timeToBreak)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (hasCollided)
+        {
+            currentTime += Time.fixedDeltaTime;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            hasCollided = true;
+        }
+    }
+}
