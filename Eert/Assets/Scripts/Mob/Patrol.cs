c5707c365323edca3a8581b65aa846cdaca872a2
syin15 <slyin@usc.edu>
2022-04-03 13:43:34 -0700

    Updated Pterodactyl

--- Eert/Assets/Scripts/Mob/Patrol.cs
+++ Eert/Assets/Scripts/Mob/Patrol.cs
@@ -8,13 +8,15 @@ public class Patrol : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float timetoFlip = 0.0f;
     [SerializeField] private float moveDistanceX;
-    [SerializeField] private float moveDistanceY;
+    //[SerializeField] private float moveDistanceY;
     [SerializeField] private float forceMuliplier = 2;
 
-    private Vector3 pointA;
-    private Vector3 pointB;
-   
+    private Vector2 pointA;
+    private Vector2 pointB;
+    private bool m_FacingRight = true;
+
     public Rigidbody2D rb;
+    public Animator animator;
    
 
 
@@ -23,7 +25,7 @@ public class Patrol : MonoBehaviour
     {
 
         pointA = rb.position;
-        pointB = new Vector3(rb.position.x + moveDistanceX, rb.position.y + moveDistanceY, 0);
+        pointB = new Vector2(rb.position.x + moveDistanceX, rb.position.y);
     }
 
     // Update is called once per frame
@@ -31,6 +33,10 @@ public class Patrol : MonoBehaviour
     { 
         Move();   
         
+        //if(Vector2.Distance(gameObject.transform.position, pointB)  == 0 || Vector2.Distance(gameObject.transform.position, pointA) == 0 )
+        //{
+        //    Flip();
+        //}
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -48,12 +54,42 @@ public class Patrol : MonoBehaviour
     {
         float time = Mathf.PingPong(Time.time * speed, 1);
         transform.position = Vector3.Lerp(pointA, pointB, time);
+        //Vector3 temp = Vector3.Lerp(pointA, pointB, time);
+        //transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+
+        Debug.Log(Vector3.Distance(transform.position, pointB));
+        if(Vector3.Distance(transform.position, pointB) < 0.3 && m_FacingRight)
+        {
+            Flip();
+            m_FacingRight = false;
+        }
+
+        if (Vector3.Distance(transform.position, pointA) < 0.3 && !m_FacingRight)
+        {
+            Flip();
+            m_FacingRight = true;
+        }
+
+
+
+
     }
 
 
     void Flip()
     {
-        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
-        speed *= -1;
+
+
+        Debug.Log("flip");
+        //transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        //speed *= -1;
+        // Switch the way the player is labelled as facing.
+        m_FacingRight = !m_FacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
