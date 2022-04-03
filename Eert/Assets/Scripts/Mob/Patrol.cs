71c8a1fd9f20528e7ab1996f1776306839580751
syin15 <slyin@usc.edu>
2022-04-03 14:48:29 -0700

    Cleaned up Pterodactyl code

--- Eert/Assets/Scripts/Mob/Patrol.cs
+++ Eert/Assets/Scripts/Mob/Patrol.cs
@@ -33,10 +33,6 @@ public class Patrol : MonoBehaviour
     { 
         Move();   
         
-        //if(Vector2.Distance(gameObject.transform.position, pointB)  == 0 || Vector2.Distance(gameObject.transform.position, pointA) == 0 )
-        //{
-        //    Flip();
-        //}
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -54,11 +50,7 @@ public class Patrol : MonoBehaviour
     {
         float time = Mathf.PingPong(Time.time * speed, 1);
         transform.position = Vector3.Lerp(pointA, pointB, time);
-        //Vector3 temp = Vector3.Lerp(pointA, pointB, time);
-        //transform.Translate(Vector2.right * speed * Time.deltaTime);
 
-
-        Debug.Log(Vector3.Distance(transform.position, pointB));
         if(Vector3.Distance(transform.position, pointB) < 0.3 && m_FacingRight)
         {
             Flip();
@@ -81,13 +73,9 @@ public class Patrol : MonoBehaviour
     {
 
 
-        Debug.Log("flip");
-        //transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
-        //speed *= -1;
-        // Switch the way the player is labelled as facing.
+     
         m_FacingRight = !m_FacingRight;
 
-        // Multiply the player's x local scale by -1.
         Vector3 theScale = transform.localScale;
         theScale.x *= -1;
         transform.localScale = theScale;
