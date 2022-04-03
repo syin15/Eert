75f2e0b182c7ebe3fc88a49c773f031d48e992ed
syin15 <slyin@usc.edu>
2022-04-02 22:24:06 -0700

    Update running animation and added Bed Reset

---
+++ Eert/Assets/Scripts/BedRest.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BedRest : MonoBehaviour
+{
+    private bool inRange = false;
+    public GameObject instruction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //if (instruction) instruction.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inRange)
+        {
+            instruction.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+        else
+        {
+            //instruction.SetActive(false);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            inRange = true;
+            Debug.Log("Trigger Enter");
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            inRange = false;
+            Debug.Log("Trigger Exit");
+        }
+    }
+
+
+}
